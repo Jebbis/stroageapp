1db360ac0180e51a18ce3306195896370b8e6f8a
Jebbis <92973916+Jebbis@users.noreply.github.com>
2023-11-11 22:03:38 +0200

    details page update

--- app/(root)/storages/[id]/page.tsx
+++ app/(root)/storages/[id]/page.tsx
@@ -1,3 +1,5 @@
+import DeliveryHistory from "@/components/cards/DeliveryHistory";
+import DetailsOverview from "@/components/cards/DetailsOverview";
 import CardDetails from "@/components/shared/CardDetails";
 
 async function page({ params }: { params: { id: string } }) {
@@ -7,6 +9,8 @@ async function page({ params }: { params: { id: string } }) {
     <div>
       <h1 className="head-text text-left">Details</h1>
       <CardDetails id={params.id} />
+      <DetailsOverview id={params.id} />
+      <DeliveryHistory id={params.id} />
     </div>
   );
 }
---
+++ types/Person.ts
@@ -0,0 +1,5 @@
+export type Person = {
+  delivery_id: string;
+  delivery_date: string;
+  delivery_note: string;
+};
